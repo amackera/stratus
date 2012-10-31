d4c9da5c316a8ee6653594084e82cac3e3db1e90
Anson MacKeracher <anson@tophatmonocle.com>
2012-10-30 23:19:33 -0400

    Only run renderer on files with 'ejs' extension

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var express = require('express');
 
 var app = express();
 
-app.engine('.html', require('ejs').__express);
 app.set('views', __dirname + '/views');
-app.set('view engine', 'html');
+app.set('view engine', 'ejs');
+app.use('/static', express.static(__dirname + '/static'));
 
 app.get('/', function(req, res) {
     res.render('index', {
