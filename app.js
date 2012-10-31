2be4cdbd83754f288a7f5f4666e9359d19b07315
Anson MacKeracher <anson@tophatmonocle.com>
2012-10-30 22:13:49 -0400

    Adds ejs rendered template

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-require('express');
+var express = require('express');
+
+var app = express();
+
+app.engine('.html', require('ejs').__express);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+
+app.get('/', function(req, res) {
+    res.render('index', {
+        str: 'hello world'
+    });
+});
+
+app.listen(process.env.PORT || 5000);
