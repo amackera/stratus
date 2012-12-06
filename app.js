9926bf362a55c239869a59c575f5c1310474aae2
Anson MacKeracher <amackera@gmail.com>
2012-12-05 23:36:01 -0500

    Set ejs open and close to '{{' and '}}' respectively

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 var express = require('express');
+var ejs = require('ejs');
+ejs.open = '{{';
+ejs.close = '}}';
 
 var app = express();
 
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.engine('ejs', ejs.__express);
 app.use('/static', express.static(__dirname + '/static'));
 
 app.get('/', function(req, res) {
-    res.render('index');
+    res.render('index.ejs');
 });
 
 app.listen(process.env.PORT || 5000);
