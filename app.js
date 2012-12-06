bf647a1604076db7d70ae1284870f23a11cadd36
Anson MacKeracher <amackera@gmail.com>
2012-12-05 22:47:05 -0500

    Remove the useless hello world render object

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ app.set('view engine', 'ejs');
 app.use('/static', express.static(__dirname + '/static'));
 
 app.get('/', function(req, res) {
-    res.render('index', {
-        str: 'hello world'
-    });
+    res.render('index');
 });
 
 app.listen(process.env.PORT || 5000);
