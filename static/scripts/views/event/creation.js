eb596726ce81ec5ad48d6d260010b5f50f3bd37b
Anson MacKeracher <anson@tophatmonocle.com>
2013-01-09 23:48:56 -0500

    Adds event/creation module

---
+++ static/scripts/views/event/creation.js
@@ -0,0 +1,29 @@
+define(['backbone', 'marionette'], function(Backbone, Marionette) {
+    return Backbone.View.extend({
+        template: '#item-creation',
+
+        events: {
+            'click input.create-event': 'create_event'
+        },
+
+        initialize: function() {
+            this.render();
+        },
+
+        render: function() {
+            var rendered_template = _.template($(this.template).html())();        
+            this.$el.append(rendered_template);
+            return this;
+        },
+
+        create_event: function(e) {
+            e.preventDefault();
+            var name = this.$el.find('.event-name').val();
+            var details = this.$el.find('.event-details').val();
+            this.collection.add({ 
+                name: name,
+                details: details
+            });
+        }
+    });
+})
