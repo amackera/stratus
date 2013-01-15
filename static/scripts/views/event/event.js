f12277d88ef0ecda8247dcfe0950f9c902fc4200
Anson MacKeracher <anson@tophatmonocle.com>
2013-01-15 01:26:43 -0500

    Adds event view implementation

--- static/scripts/views/event/event.js
+++ static/scripts/views/event/event.js
@@ -1,3 +1,11 @@
 define(['backbone', 'marionette'], function(Backbone, Marionette) {
-    return Backbone.Marionette.ItemView.extend({});
+    return Backbone.Marionette.ItemView.extend({
+        template: '#event',
+        initialize: function() {},
+        render: function() {
+            var rendered_template = _.template($(this.template).html())(this.model.attributes);
+            this.$el.append(rendered_template);
+            return this;
+        },
+    });
 });
