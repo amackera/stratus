13e23b0e30e70b9ba353b5a7316474aab153f10e
Anson MacKeracher <anson@tophatmonocle.com>
2013-02-05 13:10:49 -0500

    Adds initial API

---
+++ api/v1.js
@@ -0,0 +1,5 @@
+var app = require('../app.js');
+
+app.get('/api/v1/event', function(req, resp) {
+
+});
--- app.js
+++ app.js
@@ -3,15 +3,21 @@ var ejs = require('ejs');
 ejs.open = '{{';
 ejs.close = '}}';
 
-var app = express();
+var app = module.exports = express();
 
 app.set('views', __dirname + '/views');
 app.engine('ejs', ejs.__express);
 app.use('/static', express.static(__dirname + '/static'));
 app.disable('view cache');
 
+
+// Include all the versions of the api
+['v1'].forEach(function(ver) { require('./api/'+ver); });
+
+// Index route
 app.get('/', function(req, res) {
     res.render('index.ejs');
 });
 
+// Listen for HTTP requests
 app.listen(process.env.PORT || 5000);
