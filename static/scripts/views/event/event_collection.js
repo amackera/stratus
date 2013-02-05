29cf553561acb487a2aea1a802196888bc3271a2
Anson MacKeracher <anson@tophatmonocle.com>
2013-02-05 16:03:04 -0500

    Refactor event collection and add some extra configs

--- static/scripts/views/event/event_collection.js
+++ static/scripts/views/event/event_collection.js
@@ -1,5 +1,13 @@
-define(['backbone', 'marionette', 'views/event/event'], function(Backbone, Marionette, EventView) {
+define([
+    'backbone',
+    'marionette',
+    'views/event/event',
+    'views/event/no_event'],
+function(Backbone, Marionette, EventView, NoEventView) {
     return Backbone.Marionette.CollectionView.extend({
-        itemView: EventView
+        itemView: EventView,
+        emptyView: NoEventView,
+        className: 'event-list',
+        tagName: 'ul'
     });
 });
