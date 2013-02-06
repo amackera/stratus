1de9fdac1905d42d8252774d839c413f4f3f19f5
Anson MacKeracher <anson@tophatmonocle.com>
2013-02-06 15:16:52 -0500

    Adds GET to events API

--- api/v1.js
+++ api/v1.js
@@ -2,23 +2,35 @@ var app = require('../app.js');
 var mongo = require('mongodb');
 
 var server = new mongo.Server('localhost', process.env.DB_PORT || 27017, {auto_reconnect: true});
-var db = new mongo.Db('events', server);
+var db = new mongo.Db('events', server, {fsync: true});
 
-/*
 app.get('/api/v1/events', function(req, resp) {
+    db.collection('events', function(err, collection) {
+        collection.find().toArray(function(err, items) {
+            resp.send(items);
+        });
+    });
 });
-*/
+
+app.get('/api/v1/events/:id', function(req, resp) {
+    var id = req.params.id;
+    db.collection('events', function(err, collection) {
+        collection.findOne({'_id': new mongo.BSONPure.ObjectID(id)}, function(err, item) {
+            resp.send(item);
+        })
+    });
+});
 
 app.post('/api/v1/events', function(req, resp) {
     var event = req.body;
     console.log('adding new event: ', event);
     db.collection('events', function(err, collection) {
-        collection.insert(wine, {safe: true}, function(err, result) {
+        collection.insert(event, {safe: true}, function(err, result) {
             if (err) {
                 resp.send({'error': 'Unable to insert!'});
             } else {
                 console.log('success adding event');
-                response.send(result[0]);
+                resp.send(result[0]);
             }
         });
     });
