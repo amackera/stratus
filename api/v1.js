5b02064ff021099bdd01f4af3125c523aeffcb90
Anson MacKeracher <anson@tophatmonocle.com>
2013-02-05 21:42:54 -0500

    Adds beginnings of API for events

--- api/v1.js
+++ api/v1.js
@@ -1,5 +1,25 @@
 var app = require('../app.js');
+var mongo = require('mongodb');
 
-app.get('/api/v1/event', function(req, resp) {
+var server = new mongo.Server('localhost', process.env.DB_PORT || 27017, {auto_reconnect: true});
+var db = new mongo.Db('events', server);
 
+/*
+app.get('/api/v1/events', function(req, resp) {
+});
+*/
+
+app.post('/api/v1/events', function(req, resp) {
+    var event = req.body;
+    console.log('adding new event: ', event);
+    db.collection('events', function(err, collection) {
+        collection.insert(wine, {safe: true}, function(err, result) {
+            if (err) {
+                resp.send({'error': 'Unable to insert!'});
+            } else {
+                console.log('success adding event');
+                response.send(result[0]);
+            }
+        });
+    });
 });
