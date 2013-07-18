b9538c1199edea3a4bcdecf7c0c10074889f16d6
Anson MacKeracher <amackera@gmail.com>
2013-07-18 16:22:46 -0400

    Fix typo and add default date to event

--- static/scripts/models/event/event.js
+++ static/scripts/models/event/event.js
@@ -1,10 +1,11 @@
 define(['backbone'], function(Backbone) {
     return Backbone.Model.extend({
-        defauts: {
+        defaults: {
             title: 'New event',
             duration: 0,
             current: true,
-            description: 'New event description'
+            description: 'New event description',
+            date: new Date(Date.now())
         }
     });
 });
