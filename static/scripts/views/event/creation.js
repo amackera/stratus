38a03518bb4fef93d9570f077939195fb2fcb126
Anson MacKeracher <amackera@gmail.com>
2013-07-18 16:33:57 -0400

    Persist new event to server, and show loader while waiting

--- static/scripts/views/event/creation.js
+++ static/scripts/views/event/creation.js
@@ -10,6 +10,7 @@ define(['backbone', 'marionette'], function(Backbone, Marionette) {
 
         render: function() {
             var rendered_template = _.template($(this.template).html())();        
+            this.$el.empty();
             this.$el.append(rendered_template);
             return this;
         },
@@ -18,10 +19,19 @@ define(['backbone', 'marionette'], function(Backbone, Marionette) {
             e.preventDefault();
             var title = this.$el.find('.event-title').val();
             var description = this.$el.find('.event-description').val();
-            this.collection.add({ 
+            this.$el.html($('#loading').html());
+            this.collection.create({ 
                 title: title,
                 description: description
+            }, {
+                wait: true,
+                success: function (model, response, options) {
+                    this.render();
+                }.bind(this),
+                error: function (model, xhr, options) {
+
+                }
             });
         }
     });
-})
+});
