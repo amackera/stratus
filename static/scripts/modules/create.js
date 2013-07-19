54d9c9abfefa1639a7376ffc90751c200864b9c8
Anson MacKeracher <amackera@gmail.com>
2013-07-19 14:21:53 -0400

    Adds new Create module for admin site

---
+++ static/scripts/modules/create.js
@@ -0,0 +1,15 @@
+define([
+       'jquery',
+       'app',
+       'views/event/creation'
+], function ($, Stratus, EventCreationView) {
+    var create = Stratus.module('Events.Create', { startWithParent: false });
+    create.on('start', function (options) {
+        // Form to create new downtime events
+        var event_creation_view = new EventCreationView({
+            collection: options.event_collection
+        });
+
+        $(document).find('div#event-list').append(event_creation_view.render().$el);
+    });
+});
