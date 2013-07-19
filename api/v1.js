2e3e6b457c11f04fcb4328b30416e3d068e780ae
Anson MacKeracher <amackera@gmail.com>
2013-07-19 00:31:25 -0400

    Some basic API authorization

--- api/v1.js
+++ api/v1.js
@@ -1,5 +1,6 @@
-var app = require('../app.js');
-var mongo = require('mongodb');
+var app = require('../app.js'),
+    mongo = require('mongodb'),
+    passport = require('passport');
 
 var server = new mongo.Server('localhost', process.env.DB_PORT || 27017, {auto_reconnect: true});
 var db = new mongo.Db('events', server, {fsync: true});
@@ -17,11 +18,18 @@ app.get('/api/v1/events/:id', function(req, resp) {
     db.collection('events', function(err, collection) {
         collection.findOne({'_id': new mongo.BSONPure.ObjectID(id)}, function(err, item) {
             resp.send(item);
-        })
+        });
     });
 });
 
-app.post('/api/v1/events', function(req, resp) {
+app.post('/api/v1/events', passport.authenticate('google'), function(req, resp) {
+    // REALLY basic auth
+    if (!req.user || !req.user.role || req.user.role != 'admin') {
+        resp.status = 403;
+        resp.send('Access Denied');
+        return;
+    }
+
     var event = req.body;
     console.log('adding new event: ', event);
     db.collection('events', function(err, collection) {
@@ -37,5 +45,10 @@ app.post('/api/v1/events', function(req, resp) {
 });
 
 app.get('/api/v1/users', function(req, resp) {
-    
+    // REALLY basic auth
+    if (!req.user || !req.user.role) {
+        resp.send('anonymous');
+    } else {
+        resp.send(req.user.role);
+    }
 });
