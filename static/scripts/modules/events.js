1158320d564ff7a58f0980f18a1aa9f150329b64
Anson MacKeracher <amackera@gmail.com>
2013-07-19 14:22:12 -0400

    Don't show the creation view in the Events module

--- static/scripts/modules/events.js
+++ static/scripts/modules/events.js
@@ -2,22 +2,19 @@ define([
     'jquery',
     'app',
     'collections/event/event', 
-    'views/event/creation',
     'views/event/event_collection'
-], function($, Stratus, EventCollection, EventCreationView, EventCollectionView) {
+], function($, Stratus, EventCollection, EventCollectionView) {
     var events = Stratus.module('Events');
     events.on('start', function(options) {
         // List of downtime events
-        window.event_collection = new EventCollection();
+        this.event_collection = new EventCollection();
 
         //  TODO: REGIONS!
 
         // View for event collection
-        var event_collection_view = new EventCollectionView({ collection: event_collection });
+        var event_collection_view = new EventCollectionView({
+            collection: this.event_collection
+        });
         $(document).find('div#event-list').append(event_collection_view.render().el);
-
-        // Form to create new downtime events
-        var event_creation_view = new EventCreationView({ collection: event_collection });
-        $(document).find('div#event-list').append(event_creation_view.render().$el);
     });
 });
